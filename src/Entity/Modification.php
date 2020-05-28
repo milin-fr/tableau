0132e2f293c22a523ca032da86ecc51178bc85c3
unknown <m.ilin.fr@gmail.com>
2020-05-28 16:36:37 +0200

    modification entity

---
+++ src/Entity/Modification.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ModificationRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=ModificationRepository::class)
+ */
+class Modification
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $field;
+
+    /**
+     * @ORM\Column(type="text", nullable=true)
+     */
+    private $oldValue;
+
+    /**
+     * @ORM\Column(type="text", nullable=true)
+     */
+    private $newValue;
+
+    /**
+     * @ORM\Column(type="datetime", nullable=true)
+     */
+    private $createdAt;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getField(): ?string
+    {
+        return $this->field;
+    }
+
+    public function setField(?string $field): self
+    {
+        $this->field = $field;
+
+        return $this;
+    }
+
+    public function getOldValue(): ?string
+    {
+        return $this->oldValue;
+    }
+
+    public function setOldValue(?string $oldValue): self
+    {
+        $this->oldValue = $oldValue;
+
+        return $this;
+    }
+
+    public function getNewValue(): ?string
+    {
+        return $this->newValue;
+    }
+
+    public function setNewValue(?string $newValue): self
+    {
+        $this->newValue = $newValue;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeInterface
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(?\DateTimeInterface $createdAt): self
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+}
