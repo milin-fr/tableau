2b377430870fa50d9309af115fd7379a23b575e1
unknown <m.ilin.fr@gmail.com>
2020-05-29 11:19:39 +0200

    task crud

---
+++ src/Form/TaskType.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Task;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class TaskType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('title')
+            ->add('description')
+            ->add('createdAt')
+            ->add('updatedAt')
+            ->add('enabled')
+            ->add('user')
+            ->add('taskStatus')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Task::class,
+        ]);
+    }
+}
