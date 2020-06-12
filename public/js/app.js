c16859e1e6ea8ac76a28df1acb448357ea73c689
unknown <m.ilin.fr@gmail.com>
2020-06-12 20:35:40 +0200

    draft axios request

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,17 @@
 var app = {
     init: function() {
-        console.log("init")
+        console.log("init");
+        document.querySelectorAll(".test-test").forEach(function(link){
+          link.addEventListener("click", app.onClickBtn);
+        });
+    },
+    onClickBtn: function(event) {
+      event.preventDefault();
+      const url = this.href;
+
+      axios.get(url).then(function(response){
+        console.log(response);
+      });
     }
   };
   
